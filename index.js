21c02c89cf3f9f24da2853edcac9decf2dbbb2a4
nizhnikgleb <nizhnik.gleb@gmail.com>
2023-02-03 15:04:22 +0200

    functions

--- index.js
+++ index.js
@@ -1,44 +1,128 @@
-// Виконай додавання 0,1 і 0,2 добийся математично правильної відповіді.
-console.log(+(0.1 + 0.2).toFixed(1))
+// 1.Напиши всі можливі варіанти створення функцій.
+// function declaration
 
-// Виконай додавання рядка "1" і цифри 2 (обидві операнди повинні бути в змінних), добийся математично правильної відповіді.
-let a = "1";
-let b = 2;
+function myFunc(a, b) {
+  return a + b;
+}
 
-console.log(+a + b);
+// function expression
 
-// // Користувач вказує обсяг флешки в Гб. Програма повинна порахувати скільки файлів розміром в 820 Мб поміщається на флешку.
-const memoryDevice = prompt("Please, enter your device memory in gygabytes")
+const myFunc2 = function (a, b) {
+  return a + b;
+};
 
-console.log(`You can save up to ${Math.floor(memoryDevice*1000/820)} files of 820MB in size`)
+// arrow function
+const myFunc3 = (a, b) => a + b;
 
-// Користувач вводить суму грошей в гаманці і ціну однієї шоколадки. Програма виводить скільки шоколадок може купити користувач і скільки здачі у нього залишиться.
-const moneyAmount = prompt("How much money do you have?");
-const chocolatePricePerOne = prompt("Please, enter the price of chocolate bar")
+// 2.Створи функцію, яка буде виводити кількість переданих їй аргументів.
+function getArgumentsNumber() {
+  return arguments.length;
+}
+console.log(getArgumentsNumber(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-console.log(`You can afford ${Math.floor(moneyAmount/chocolatePricePerOne)} chocolate bars and amount of ${moneyAmount%chocolatePricePerOne} hrns will stay in your pocket`)
+// 3.Напиши функцію, яка приймає 2 числа і повертає :
+// -1, якщо перше число менше, ніж друге;
+// 1 - якщо перше число більше, ніж друге;
+// 0 - якщо числа рівні.
+function compareNumbers(a, b) {
+  if (arguments.length < 2) {
+    return `You need to enter 2 arguments`;
+  }
 
-// Запитай у користувача тризначне число і виведи його задом наперед. Для вирішення завдання тобі знадобиться оператор % (залишок від ділення).
+  for (let i of arguments) {
+    if (i === "" || i === " " || isNaN(i)) {
+      return "invalid data";
+    }
+  }
+  return a < b ? -1 : a > b ? 1 : 0;
+}
+console.log(compareNumbers(10, 9));
 
-// first option
-const number = +prompt("Please, enter three-digit number");
-const reversedNumber = `${number % 10}${Math.floor(number / 10) % 10}${Math.floor(number / 100) % 10}`;
-console.log(reversedNumber);
+// 4. Напиши функцію, яка обчислює факторіал переданого їй числа.
+function calculateFactorial(a) {
+  if (!a || a === " " || isNaN(a)) return `invalid data`;
 
-// // second option
-const numberEnteredByUser = +prompt("Please, enter three-digit number");
-const firstDigit = (numberEnteredByUser % 10) * 100;
-const secondDigit = (Math.floor(numberEnteredByUser / 10) % 10) * 10;
-const thirdDigit = Math.floor(numberEnteredByUser / 100);
-const reversedNumber2 = firstDigit + secondDigit + thirdDigit;
-console.log(reversedNumber2);
+  return a === 1 ? 1 : a * calculateFactorial(a - 1);
+}
+console.log(calculateFactorial(5));
 
-// Користувач вводить суму вкладу в банк на 2 місяці, з процентною ставкою депозиту 5% річних. Вивести суму нарахованих відсотків.
-let depositAmount = prompt('Please enter amount of the deposit');
-let depositRatePerYear = 0.05;
-let depositTerm = 2;
-let amountOfAccruedInterest = (depositAmount * depositRatePerYear / 12).toFixed(2) * depositTerm;
+// 5. Напиши функцію, яка приймає три окремі цифри і перетворює їх в одне число. Наприклад: цифри 1, 4, 9 перетворяться в число 149.
+function getConcatNumber(a = 0, b = 0, c = 0) {
+  for (let i of arguments) {
+    if (i === "" || i === " " || isNaN(i)) {
+      return "invalid data";
+    }
+  }
+  return a * 100 + b * 10 + c;
+  //   alternative option  +(`${a}${b}${c}`)
+}
+console.log(getConcatNumber(7, 2, 1));
 
-console.log(`You will get amount of ${amountOfAccruedInterest} dollars after ${depositTerm} months`)
+// 6. Напиши функцію, яка приймає довжину і ширину прямокутника і обчислює його площу. Якщо в функцію передали 1 параметр, то вона обчислює площу квадрата.
 
+function getSquare(a, b) {
+  for (let i of arguments) {
+    if (i === "" || i === " " || isNaN(i)) {
+      return "invalid data";
+    }
+  }
+  return a * (b || a);
+}
+console.log(getSquare(2, 12));
 
+// 7. Напиши функцію, яка перевіряє, чи є передане їй число “досконалим числом”. Досконале число - це число, яке дорівнює сумі всіх своїх дільників.
+
+function getPerfectNumber(n) {
+  let sumOfAllDividers = 0;
+
+  if (n === "" || n === " " || isNaN(n)) {
+    return "invalid data";
+  }
+
+  for (let i = 1; i < n; i++) {
+    if (!(n % i)) {
+      sumOfAllDividers += i;
+    }
+  }
+
+  return sumOfAllDividers == n
+    ? "Your number is pefect"
+    : "Your number is not perfect";
+}
+console.log(getPerfectNumber(28));
+
+// 8.Напиши функцію, яка приймає мінімальне і максимальне значення для діапазону, і виводить тільки ті числа з діапазону, які є досконалими. Використовуй написану раніше функцію, щоб дізнатися, чи є це число досконалим.
+
+function findPerfectNumber(i) {
+  let sumOfAllDividers = 0;
+  for (let j = 1; j < i; j++) {
+    if (!(i % j)) {
+      sumOfAllDividers += j;
+    }
+  }
+
+  if (sumOfAllDividers == i) {
+    return i;
+  }
+}
+
+function getAllPerfectNumbers(a, b) {
+  let arr = [];
+
+  if (arguments.length < 2) return "you need to enter two numbers";
+
+  for (let i of arguments) {
+    if (i === "" || i === " " || isNaN(i)) {
+      return "invalid data";
+    }
+  }
+
+  for (let i = a; i <= b; i++) {
+    let number = findPerfectNumber(i);
+    if (number) {
+      arr.push(number);
+    }
+  }
+  return arr.join(", ");
+}
+console.log(getAllPerfectNumbers(1, 10000));
