189aab64f05f42c079f5fe496556daf463a91f2a
nizhnikgleb <nizhnik.gleb@gmail.com>
2023-02-10 10:39:25 +0200

    objects/tasks

--- index.js
+++ index.js
@@ -1,110 +1,98 @@
-// 1. Створи масив «Список покупок». Кожен елемент масиву є об'єктом, який містить назву продукту, кількість і куплений він чи ні, ціну за одиницю товару, сума. Написати кілька функцій для роботи з таким масивом:
-const shoppingList = [
-  { name: "milk", quantity: 3, isBought: true, pricePerOne: 30, sum: 90 },
-  { name: "potatoes", quantity: 4, isBought: false, pricePerOne: 10, sum: 40 },
-  { name: "oranges", quantity: 3, isBought: true, pricePerOne: 10, sum: 30 },
-  { name: "apples", quantity: 1, isBought: false, pricePerOne: 20, sum: 20 },
-  {
-    name: "chocolates",
-    quantity: 4,
-    isBought: false,
-    pricePerOne: 50,
-    sum: 200,
+// 1. Створи об'єкт, що описує автомобіль (виробник, модель, рік випуску, середня швидкість, обсяг паливного баку, середня витрата палива на 100 км., водії), і наступні методи для роботи з цим об'єктом:
+const car = {
+  manufacturer: "Volkswagen",
+  model: "Passat",
+  year: 2020,
+  averageSpeed: 120,
+  fuelCapacity: 100,
+  averageConsumption: 8,
+  drivers: ["John", "Mark", "Steven"],
+  // 1.1 Метод, який виводить на екран інформацію про автомобіль.
+  getCarInfo() {
+    for (let [key, value] of Object.entries(this)) {
+      if (typeof value === "function") continue;
+      console.log(`${key}: ${value}`);
+    }
   },
-  { name: "cheese", quantity: 2, isBought: false, pricePerOne: 50, sum: 100 },
-  { name: "water", quantity: 10, isBought: false, pricePerOne: 15, sum: 150 },
-];
-// 1.1 Виводити весь список на екран таким чином, щоб спочатку йшли продукти, що ще не придбані, а потім - ті, що вже придбали.
-function sortShoppingListFromUnbought() {
-  const arrayOfProductsFromUnbought = [...shoppingList].sort(
-    (a, b) => a.isBought - b.isBought
-  );
-  return arrayOfProductsFromUnbought;
-}
-// console.log(sortShoppingListFromUnbought());
+  // 1.2. Додавання ім’я водія у список
+  addDriverName(driverName) {
+    this.drivers.push(driverName);
+  },
+  // 1.3 Перевірка водія на наявність його ім’я у списку
+  checkDriverInList(driverName) {
+    if (this.drivers.includes(driverName)) {
+      console.log(`${driverName} is already in the list`);
+    } else {
+      if (
+        confirm(
+          `Driver ${driverName} is not in the list. Would you like to add driver?`
+        )
+      ) {
+        this.addDriverName();
+      } else return;
+    }
+  },
+  // 4. Підрахунок необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину. Підрахунок необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину.
+  calculateTimeAndFuel(distance) {
+    let hours = Math.floor(distance / this.averageSpeed);
+    let minutes = Math.trunc(
+      ((distance / this.averageSpeed).toFixed(2) - hours) * 60
+    );
 
-//1.2 Покупка продукту. Функція приймає назву продукту і відзначає його як придбаний.
-function changeStatusOfPurchase(productName) {
-  let indexNumber = shoppingList.findIndex((el) => el.name === productName);
+    let hoursForRest = Math.floor(hours / 4);
+    hours += hoursForRest;
 
-  if(indexNumber === -1) {
-    console.log(`${productName} doesn't exist in the Shopping list`)
-  }
+    hours = hours ? hours + " hours " : "";
+    minutes = minutes ? minutes + " minutes " : "";
 
-  shoppingList[indexNumber].isBought = true;
-}
-// changeStatusOfPurchase("apples");
+    let fuelQuantity = Math.round((distance / 100) * this.averageConsumption);
 
-// Видалення продукту зі списку (видалення повинно проводитися шляхом створення нового масиву, в якому продукт, що ми шукаємо, буде відсутнім)
-function deletePurchaseItem(productName) {
-  const newArr = [...shoppingList].filter((el) => el.name !== productName);
-  return newArr;
-}
-// deletePurchaseItem("cheese");
+    return `You need ${hours}${minutes}and about ${fuelQuantity} liters of fuel`;
+  },
+};
+// console.log(car.calculateTimeAndFuel(340));
 
-// 2. Додавання покупки в список. Враховуй, що при додаванні покупки з уже існуючим в списку продуктом, необхідно збільшувати кількість в існуючій покупці, а не додавати нову. При цьому також повинна змінитися сума, наприклад, якщо ціна за одиницю 12, а кількості товарів стало 2, то сума буде 24.
-function addPurchaseItemtoList(productName) {
-  if(!isNaN(productName) || typeof productName !== "string" ) {
-    return "invalid data"
-  }
-  
-    const indexOfProduct = shoppingList.findIndex(
-    (el) => el.name === productName
-  );
-  if (indexOfProduct >= 0) {
-    ++shoppingList[indexOfProduct].quantity;
-    shoppingList[indexOfProduct].sum +=
-      shoppingList[indexOfProduct].pricePerOne;
-    console.log(shoppingList);
-  } else {
-    const pricePerItem = +prompt("How much does your new item cost?");
-    let newPurchaseItem = {
-      name: productName,
-      quantity: 1,
-      isBought: false,
-      pricePerOne: pricePerItem,
-      sum: pricePerItem,
-    };
-    shoppingList.push(newPurchaseItem);
-  }
-}
-// addPurchaseItemtoList("tomatoes");
+// 2. Створити об'єкт, що описує час (години, хвилини, секунди), і такі функції для роботи з цим об'єктом:
+const time = {
+  hours: 8,
+  minutes: 4,
+  seconds: 15,
+  // 2.1 Для виведення часу на екран
+  showTime() {
+    this.hours = this.hours < 10 ? "0" + (+this.hours) : this.hours;
+    this.minutes = this.minutes < 10 ? "0" + (+this.minutes) : this.minutes;
+    this.seconds = this.seconds < 10 ? "0" + (+this.seconds) : this.seconds;
+    alert(`${this.hours}:${this.minutes}:${this.seconds}`);
+  },
+  // 2.2 Зміни часу на передану кількість секунд.
+  addSeconds(sec) {
+    this.seconds += sec;
+    this.minutes =  +this.minutes + Math.floor(this.seconds / 60);
 
-// 1. Підрахунок суми всіх продуктів (враховуючи кількість кожного) в списку.
-function getSumOfAllPurchases() {
-  const sumOfAllPurchases = shoppingList.reduce(
-    (total, item) => total + item.sum,
-    0
-  );
+    if (this.minutes >= 60) {
+      this.hours = +this.hours + Math.floor(this.minutes / 60);
+      this.minutes = this.minutes - Math.floor(this.minutes / 60) * 60;
+    }
+    this.seconds = this.seconds - Math.floor(this.seconds / 60) * 60;
+    time.showTime();
+  },
+  // 2.3 Зміни часу на передану кількість хвилин.
+  addMinutes(min) {
+    this.minutes = +this.minutes + min;
+    this.hours = +this.hours + Math.floor(this.minutes / 60);
+    if (this.hours >= 24) {
+      this.hours = this.hours - Math.floor(this.hours / 24) * 24;
+    }
 
-  return sumOfAllPurchases;
-}
-// console.log(getSumOfAllPurchases())
+    this.minutes = this.minutes - Math.floor(this.minutes / 60) * 60;
+    this.showTime();
+  },
+  // 2.4. Зміни часу на передану кількість годин.
+  addHours(hours) {
+    this.hours += hours;
+    this.hours = this.hours - Math.floor(this.hours / 24) * 24;
+    // this.minutes = this.minutes - Math.floor(this.minutes / 60) * 60
+    this.showTime();
+  },
+};
 
-// 2. Підрахунок суми всіх (не) придбаних продуктів.
-function getSumOfAllUnpurchasedProducts() {
-  const arrayOfAllUnboughtProducts = shoppingList.filter(
-    (el) => el.isBought === false
-  );
-  const sumOfAllUnboughtProducts = arrayOfAllUnboughtProducts.reduce(
-    (total, item) => total + item.sum,
-    0
-  );
-  return sumOfAllUnboughtProducts;
-}
-// console.log(getSumOfAllUnpurchasedProducts());
-
-// 3.Показання продуктів в залежності від суми, (від більшого до меншого / від меншого до більшого, в залежності від параметра функції, який вона приймає)
-function sortShoppingList() {
-  const criteria = confirm(
-    "Would you like to see shopping list from biggest sum to lower?"
-  );
-
-  const sortedArray = [...shoppingList];
-
-  return criteria
-    ? sortedArray.sort((a, b) => b.sum - a.sum)
-    : sortedArray.sort((a, b) => a.sum - b.sum);
-}
-
-// console.log(sortShoppingList());
